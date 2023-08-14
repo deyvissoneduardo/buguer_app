3a0d0d1730f6815e44541ac80f3da2c39c9c0801
deyvissoneduardo <deyvissoneduardo22@gmail.com>
2023-08-14 15:47:23 -0300

    models and dtos

---
+++ lib/src/models/order/order_status.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+enum OrderStatus {
+  pendente('Pendente', 'P', Colors.blue),
+  confirmado('Confirmado', 'C', Colors.green),
+  finalizado('Finalizado', 'F', Colors.black),
+  cancelado('Cancelado', 'R', Colors.red);
+
+  final String name;
+  final String acronym;
+  final Color color;
+
+  const OrderStatus(this.name, this.acronym, this.color);
+
+  static OrderStatus parse(String acronym) =>
+      values.firstWhere((s) => s.acronym == acronym);
+}
---
+++ lib/src/models/user_model.dart
@@ -0,0 +1,43 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
+class UserModel {
+  final int? id;
+  final String name;
+  final String email;
+  final String cpf;
+  final String password;
+
+  UserModel({
+    this.id,
+    required this.name,
+    required this.email,
+    required this.cpf,
+    required this.password,
+  });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'name': name,
+      'email': email,
+      'cpf': cpf,
+      'password': password,
+    };
+  }
+
+  factory UserModel.fromMap(Map<String, dynamic> map) {
+    return UserModel(
+      id: map['id'] != null ? map['id'] as int : null,
+      name: map['name'] as String,
+      email: map['email'] as String,
+      cpf: map['cpf'] as String,
+      password: map['password'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory UserModel.fromJson(String source) =>
+      UserModel.fromMap(json.decode(source) as Map<String, dynamic>);
+}
