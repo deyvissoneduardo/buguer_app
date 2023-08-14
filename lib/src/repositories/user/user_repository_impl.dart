c0cde1cfa10249f4e85b75cacde67a7a4cd0fd58
deyvissoneduardo <deyvissoneduardo22@gmail.com>
2023-08-14 15:52:58 -0300

    implemented user repository

---
+++ lib/src/repositories/user/user_repository_impl.dart
@@ -0,0 +1,25 @@
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
+
+import '../../core/exception/respository_exception.dart';
+import '../../core/rest_client/custom_dio.dart';
+import '../../models/user_model.dart';
+import 'user_repository.dart';
+
+class UserRepositoryImpl implements UserRepository {
+  final CustomDio _dio;
+
+  UserRepositoryImpl(this._dio);
+
+  @override
+  Future<UserModel> getById(int id) async {
+    try {
+      final response = await _dio.auth().get('/users/$id');
+      return UserModel.fromMap(response.data);
+    } on DioException catch (e, s) {
+      log('Error ao buscar usuario', error: e, stackTrace: s);
+      throw const RespositoryException(message: 'Error ao buscar usuario');
+    }
+  }
+}
